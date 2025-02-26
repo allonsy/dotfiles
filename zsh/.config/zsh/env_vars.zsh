ffa1d87717934e8c08e612e8350cd2a9f8e19efa
Alec Snyder <alec.snyder@at-bay.com>
2025-02-26 16:01:48 +0200

    Add jj.gc and cargo install root Adds a new jj gc script to clean jj repos Adds cargo install root to be on the local path

--- zsh/.config/zsh/env_vars.zsh
+++ zsh/.config/zsh/env_vars.zsh
@@ -5,4 +5,5 @@ NVM_DIR=~/.nvm
 NVM_CD_FLAGS=-q
 NVM_BIN=~/.nvm/versions/node/v14.18.1/bin
 NVM_INC=~/.nvm/versions/node/v14.18.1/include/node
+CARGO_INSTALL_ROOT=~/.local
 export PATH=~/.nvm/versions/node/v14.18.1/bin:~/.local/bin:$PATH
